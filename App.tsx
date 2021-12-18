4acec9910318c422c1860bf452087d02beb9422f
gu807 <gugamelo2@gmail.com>
2021-12-18 00:42:03 -0300

    signin

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import React from "react";
+import {Text, View, StyleSheet} from 'react-native'
 
-export default function App() {
+
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Alo mundo</Text>
+      <StatusBar style='auto'/>
     </View>
-  );
-}
+  )
+} 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  container : {
+    flex : 1,
+    backgroundColor: '#FFF',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent : 'center',
   },
 });
